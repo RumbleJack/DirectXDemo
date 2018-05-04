91fbc05d8457170cad040e10a8b373fc22421ee5
renjiewen <renjiewen1995@163.com>
2018-05-04 17:29:03 +0800

    staging

---
+++ MineEngine/ProjectForComputerGraphics/Common/GlobalConfig.h
@@ -0,0 +1,8 @@
+#pragma once
+// 全局变量 
+const bool	FULL_SCREEN = false;
+const bool	VSYNC_ENABLED = true;	//垂直同步
+const float SCREEN_DEPTH = 500.0f;
+const float SCREEN_NEAR = 1.0f;
+const bool MSAA4X_ENABLED = true;
+
--- MineEngine/ProjectForComputerGraphics/stdafx.h
+++ MineEngine/ProjectForComputerGraphics/stdafx.h
@@ -1,3 +1,36 @@
-#pragma once
+#pragma once 
+//#define DIRECTX_2010
+
+#include "PubMacroDef.h"
+#include "GlobalConfig.h"
+#include <d3d11.h>
+#include <dxgi.h>
+
+#ifdef DIRECTX_2010
+// 禁止按16字节对齐
+#define _XM_NO_INTRINSICS_
+#define XM_NO_ALIGNMENT
+#include <xnamath.h>
+struct DllExport _XMFLOAT4;
+struct DllExport _XMMATRIX;
+#include <d3dx11Async.h>
+//// 链接
+	#pragma comment(lib, "d3d11.lib")   
+	#pragma comment(lib, "dxgi.lib")	 
+	#pragma comment(lib, "d3dx11.lib")   
+#else
+//在Win8及以上平台可用
+#include <directxmath.h>
+#include <d3dcompiler.h>
+using namespace DirectX;
+//// 链接
+	#pragma comment(lib, "d3d11.lib")		// contains all the Direct3D functionality
+	#pragma comment(lib, "dxgi.lib")	 //contains tools to interface with the hardware
+	#pragma comment(lib, "d3dcompiler.lib")  //contains functionality for compiling shaders 
+#endif
+
+#define MEMCPY_MATRIX(A,B) memcpy(reinterpret_cast<float*>(A.m), reinterpret_cast<float*>(B.m), 16 * sizeof(float))
+
+
 
 
